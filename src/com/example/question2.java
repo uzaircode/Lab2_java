c6bfc3bce0c7e0693386f5dad5c0fb5b97d26c1d
uzaircode <nikuzairsc@gmail.com>
2022-01-25 13:17:15 +0800

    final commit

--- src/com/example/question2.java
+++ src/com/example/question2.java
@@ -8,7 +8,7 @@ public class question2 {
 
         double height=0, weight=0;
 
-        double bmi;
+        double bmi = 0;
 
         String category="";
 
@@ -17,23 +17,36 @@ public class question2 {
 
         for(int i = 0; i<num; i++)
         {
-            System.out.println("Enter weight and height for student " + ++i + ": ");
+            System.out.println("Enter weight and height for student " + (i+1) + ": ");
             System.out.print("Weight (lbs) = ");
             weight = input.nextDouble();
             System.out.print("Height (in) = ");
             height = input.nextDouble();
+
+            bmi += ((weight * 703)/(height*height)/num);
+
         }
-
-        bmi = (weight * 703)/(height*height);
-
-        System.out.println("Average BMI for this class is : " + bmi );
+        
+        System.out.printf("\nAverage BMI for this class is : %.2f" , bmi);
 
         if(bmi < 18.5)
         {
             category = "Underweight";
         }
+        else if(bmi >= 18.5 && bmi < 25)
+        {
+            category = "Normal";
+        }
+        else if(bmi >= 25 && bmi < 30)
+        {
+            category = "Overweight";
+        }
+        else if(bmi >= 30)
+        {
+            category = "Obese";
+        }
 
-        System.out.println("Majority the students of this class are: ");
+        System.out.println("\nMajority the students of this class are: " + category);
 
 
 
